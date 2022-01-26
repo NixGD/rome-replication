7d961e56bc64bdbb0b601ffacc89e896a3ac6e79
Nicholas Goldowsky-Dill <8730377+NixGD@users.noreply.github.com>
2022-01-26 22:43:53 +0000

    small changes

--- rome.py
+++ rome.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 import warnings
 import torch as t
-from tqdm import tqdm
+import tqdm
 
 import datasets
 
@@ -20,7 +20,7 @@ def estimate_C(model: GPT2, layer):
     with HookHandler() as hh:
         hh.add_save_input_hook(linear)
 
-        for input_ids in tqdm(dataset['input_ids'][:1000]):
+        for input_ids in tqdm.tqdm(dataset['input_ids'][:2000]):
             if input_ids.shape[0] > 0:
                 input_ids = input_ids.unsqueeze(0).to(get_device(model))
                 model(input_ids)
@@ -42,22 +42,25 @@ def get_C(model, layer):
         return cached
 
 
-def get_k_star_and_z0(model: GPT2, layer, fact):
+def get_k_star_and_z0(model: GPT2, layer, fact, subj_pos):
     linear = model.blocks[layer].linear2
 
     with HookHandler() as hh:
         hh.add_save_input_output_hook(linear)
         input_ids = encode_for_model(model, fact.subject)
         model(input_ids)
-        return hh.inputs[0][0,-1,:], hh.outputs[0][0,-1,:]
+        return hh.inputs[0][0,subj_pos,:], hh.outputs[0][0,subj_pos,:]
 
 
-def get_z_star(model: GPT2, layer: int, fact: Fact, new_obj: str, z0: t.Tensor):
+def get_z_star(model: GPT2, layer: int, fact: Fact, new_obj: str, z0: t.Tensor, subj_pos = -1):
     tokenizer = model.tokenizer
     input_ids, subj_len, _ = fact_tensors(fact, tokenizer, get_device(model))
     new_obj_id = tokenizer.encode(new_obj, return_tensors="pt")
     assert len(new_obj_id) == 1
     new_obj_id = new_obj_id[0].to(z0.device)
+
+    if subj_pos < 0:
+        subj_pos = subj_len + subj_pos + 1
 
     z = z0.clone().detach()
     z.requires_grad = True
@@ -65,7 +68,7 @@ def get_z_star(model: GPT2, layer: int, fact: Fact, new_obj: str, z0: t.Tensor):
     with tqdm.trange(50) as thandle:
         for step in thandle:
             optim.zero_grad()
-            patch = Patch("mlp", subj_len, layer, z)
+            patch = Patch("mlp", subj_pos, layer, z)
             out = model.forward_corrupt_and_patch(input_ids, patch=patch)
             new_obj_prob = out.logits.softmax(dim=-1)[0, new_obj_id]
             loss = -t.log(new_obj_prob)
@@ -100,14 +103,14 @@ def calcuate_new_weights(W: t.Tensor, C: t.Tensor, k_star: t.Tensor, v_star: t.T
     return W_hat
 
 
-def rome(model: GPT2, fact: Fact,  new_obj: str, layer: int, subject_pos: int = -1):
+def rome(model: GPT2, fact: Fact,  new_obj: str, layer: int, subj_pos: int = -1):
     linear = model.blocks[layer].linear2
     W = linear.weight
     print("Estimating C")
     C = get_C(model, layer)
-    k_star, z0 = get_k_star_and_z0(model, layer, fact)
+    k_star, z0 = get_k_star_and_z0(model, layer, fact, subj_pos)
     print("Estimating v_star")
-    v_star = get_z_star(model, layer, fact, new_obj, z0)
+    v_star = get_z_star(model, layer, fact, new_obj, z0, subj_pos)
 
     W_hat = calcuate_new_weights(W, C, k_star, v_star)
     return W_hat
--- utils.py
+++ utils.py
@@ -4,13 +4,18 @@ import torch as t
 
 Corruption = namedtuple("Corruption", ["end_position", "noise_std"], defaults=[0.1])
 Patch = namedtuple("Patch", ["type", "token", "layer", "value"])
-Fact = namedtuple("Fact", ['subject', 'relation', 'object'])
+Fact = namedtuple("Fact", ["subject", "relation", "object"])
+
+from hook_handler import HookHandler
+
 
 def get_device(model):
     return next(model.parameters()).device
 
+
 def encode_for_model(model, text):
     return model.tokenizer.encode(text, return_tensors="pt").to(get_device(model))
+
 
 def fact_tensors(fact, tokenizer, device):
     if fact.relation[0] != " ":
@@ -32,6 +37,7 @@ def fact_tensors(fact, tokenizer, device):
 
     return input_ids, subj_len, correct_id
 
+
 def most_likely(model, fact, k=5):
     input_ids, _, _ = fact_tensors(fact, model.tokenizer, device=get_device(model))
 
@@ -42,11 +48,57 @@ def most_likely(model, fact, k=5):
         token = model.tokenizer.decode(top_ids[i])
         print(f"{repr(token).ljust(15)}{top_probs[i]:.2%}")
 
+
 def get_correct_prob(out, correct_id):
     return t.softmax(out.logits[0], dim=-1)[correct_id].item()
+
 
 def print_tokenized(ids, tokenizer):
     l = tokenizer.batch_decode([[id] for id in ids])
     for t in l:
         print(repr(t), end=" ")
     print()
+
+
+def get_edit_hook(W_hat):
+    def hook(model, input, output):
+        x = t.einsum("eh,bsh->bse", W_hat, input[0])
+        return x + model.bias
+
+    return hook
+
+
+def compare_most_likely(model, fact, layer, W_hat):
+    print("Unedited:")
+    most_likely(model, fact)
+
+    with HookHandler() as hh:
+        linear = model.blocks[layer].linear2
+        hh.add_hook(linear, get_edit_hook(W_hat))
+
+        print("Editied:")
+        most_likely(model, fact)
+
+
+def compare_generated(model, prompt, layer, W_hat, num=5, temperature=1, max_length=20):
+    device = get_device(model)
+
+    def print_generated():
+        generated = model.generate(
+            prompt,
+            device=device,
+            temperature=temperature,
+            max_length=max_length,
+        )
+        print(repr(generated), end="\n\n")
+
+    print("=== Unmodified ===")
+    for i in range(num):
+        print_generated()
+
+    print("=== Modified ===")
+    with HookHandler() as hh:
+        layer = model.blocks[layer].linear2
+        hh.add_hook(layer, get_edit_hook(W_hat))
+        for i in range(num):
+            print_generated()
